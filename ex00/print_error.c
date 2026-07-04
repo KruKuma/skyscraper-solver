5f5b638818c6ad8f422d5a09881cdf3414251fcf
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-04 10:33:19 +0200

    feat(print_error): add error message function

--- ex00/print_error.c
+++ ex00/print_error.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/04 09:54:47 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/04 10:29:25 by nfurst           ###   ########.fr       */
+/*   Created: 2026/07/04 10:28:04 by nfurst            #+#    #+#             */
+/*   Updated: 2026/07/04 10:30:34 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	print_error();
-
-int main(int argc, char *argv[])
+void	print_error()
 {
-	int grid[4][4];
-	int views[16];
-
-	if (argc != 2)
-	{
-		print_error();
-		return (1);
-	}
-	return (0);
+	write(1, "Error\n", 6);
 }
