607377b824049e956694345861c7f1e97c232ef4
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-04 15:24:17 +0200

    feat(check): add a function to count the block visible

--- ex00/check.c
+++ ex00/check.c
@@ -6,13 +6,29 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/04 14:49:32 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/04 14:54:14 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/04 15:22:38 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int count_visible(int line[4])
+int	count_visible(int line[4])
 {
-	return (1);
+	int	i;
+	int	max;
+	int	count;
+
+	i = 0;
+	max = 0;
+	count = 0;
+	while (i < 4)
+	{
+		if (line[i] > max)
+		{
+			max = line[i];
+			count++;
+		}
+		i++;
+	}
+	return (count);
 }
 
 int	check_row(int grid[4][4], int row, int left, int right)
