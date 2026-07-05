4cd5ea7ae1b09c326c20b47d81e1a6648a13bd79
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-05 08:20:45 +0200

    fix(check): reset check after checking top and left

--- ex00/check.c
+++ ex00/check.c
@@ -44,6 +44,7 @@ int	check_row(int grid[4][4], int row, int left, int right)
 	}
 	if (count_visible(line) != left)
 		return (0);
+	i = 0;
 	while (i < 4)
 	{
 		line[i] = grid[row][3 - i];
@@ -67,6 +68,7 @@ int	check_col(int grid[4][4], int col, int top, int bottom)
 	}
 	if (count_visible(line) != top)
 		return (0);
+	i = 0;
 	while (i < 4)
 	{
 		line[i] = grid[3 - i][col];
