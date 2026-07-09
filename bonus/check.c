5835803bdad1d7451b31324baebf04b21edb5992
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-09 03:17:57 +0200

    feat(bonus/check): add support for 9x9

--- bonus/check.c
+++ bonus/check.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	count_visible(int line[4])
+int	count_visible(int line[9], int size)
 {
 	int	i;
 	int	max;
@@ -19,7 +19,7 @@ int	count_visible(int line[4])
 	i = 0;
 	max = 0;
 	count = 0;
-	while (i < 4)
+	while (i < size)
 	{
 		if (line[i] > max)
 		{
@@ -31,64 +31,64 @@ int	count_visible(int line[4])
 	return (count);
 }
 
-int	check_row(int grid[4][4], int row, int left, int right)
+int	check_row(int grid[9][9], int row, int left, int right, int size)
 {
-	int	line[4];
+	int	line[9];
 	int	i;
 
 	i = 0;
-	while (i < 4)
+	while (i < size)
 	{
 		line[i] = grid[row][i];
 		i++;
 	}
-	if (count_visible(line) != left)
+	if (count_visible(line, size) != left)
 		return (0);
 	i = 0;
-	while (i < 4)
+	while (i < size)
 	{
-		line[i] = grid[row][3 - i];
+		line[i] = grid[row][size - 1 - i];
 		i++;
 	}
-	if (count_visible(line) != right)
+	if (count_visible(line, size) != right)
 		return (0);
 	return (1);
 }
 
-int	check_col(int grid[4][4], int col, int top, int bottom)
+int	check_col(int grid[9][9], int col, int top, int bottom, int size)
 {
-	int	line[4];
+	int	line[9];
 	int	i;
 
 	i = 0;
-	while (i < 4)
+	while (i < size)
 	{
 		line[i] = grid[i][col];
 		i++;
 	}
-	if (count_visible(line) != top)
+	if (count_visible(line, size) != top)
 		return (0);
 	i = 0;
-	while (i < 4)
+	while (i < size)
 	{
-		line[i] = grid[3 - i][col];
+		line[i] = grid[size - 1 - i][col];
 		i++;
 	}
-	if (count_visible(line) != bottom)
+	if (count_visible(line, size) != bottom)
 		return (0);
 	return (1);
 }
 
-int	check_all_views(int grid[4][4], int views[16])
+int	check_all_views(int grid[9][9], int views[36], int size)
 {
 	int	i;
 
 	i = 0;
-	while (i < 4)
+	while (i < size)
 	{
-		if (!check_col(grid, i, views[i], views[i + 4]))
+		if (!check_col(grid, i, views[i], views[i + size], size))
 			return (0);
-		if (!check_row(grid, i, views[i + 8], views[i + 12]))
+		if (!check_row(grid, i, views[i + size * 2], views[i + size * 3], size))
 			return (0);
 		i++;
 	}
